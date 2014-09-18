842a1e91fc7cbb7ec384a51a846f5b6b1ec283a1
Andy Mastbaum <amastbaum@gmail.com>
2014-09-18 16:26:01 -0700

    find components by name in the decaymanager

--- src/gen/DecayManager.cc
+++ src/gen/DecayManager.cc
@@ -1,7 +1,10 @@
+#include <string_utilities.hpp>
 #include <G4Event.hh>
 #include <Randomize.hh>
 #include <RAT/DecayManager.hh>
 #include <RAT/DecayManagerMessenger.hh>
+#include <RAT/DetectorConstruction.hh>
+#include <RAT/DetectorComponent.hh>
 #include <RAT/Log.hh>
 #include <RAT/GLG4TimeGen.hh>
 
@@ -19,7 +22,10 @@ DecayManager::~DecayManager() {
 }
 
 
-void DecayManager::GenerateEvent(G4Event *event) {}
+void DecayManager::GenerateEvent(G4Event *event) {
+  // TODO: Draw events from a time-ordered series of decays drawn from
+  // all DetectorComponents registered in DetectorConstruction.
+}
 
 
 void DecayManager::ResetTime(double offset) {
@@ -30,8 +36,21 @@ void DecayManager::ResetTime(double offset) {
 void DecayManager::SetComponentLoad(std::string volumeName,
                                     std::string chain,
                                     double activity) {
-  warn << "Setting load on component " << volumeName << ": "
+  warn << "DecayManager: Setting load on component " << volumeName << ": "
        << activity << " Bq of " << chain << newline;
+
+  DetectorConstruction* detector =
+    DetectorConstruction::GetDetectorConstruction();
+
+  DetectorComponent* component = detector->GetComponents()[volumeName];
+
+  Log::Assert(component, dformat("DecayManager: Volume %s not found",
+                                 volumeName.c_str()));
+
+  warn << "Found the component " << volumeName
+       << " at " << component << newline;
+
+  // TODO: Set the activity in the DetectorComponent
 }
 
 
